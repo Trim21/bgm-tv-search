8f74bc03504619f170b5782d86c67c7b6c582799
Trim21 <i@trim21.me>
2019-12-09 18:25:53 +0800

    style

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,13 +1,10 @@
 import { expect } from 'chai'
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import About from '@/views/About.vue'
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).to.include(msg)
+    const wrapper = shallowMount(About)
+    expect(wrapper.text()).to.include('some of mine api')
   })
 })
